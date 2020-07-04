c38d3a095381bd6d4e53df9e7c5c070a916e949f
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-07-05 00:03:22 +0530

    mux handler

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/udayinbiswas21/covid_service/external"
 )
 
-func main() {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	res, err := external.GetCrowdSourcedData()
 	if err != nil {
 		log.Println(err)
 	}
-	log.Println("Result is", res)
-	log.Println("Result length is", len(res.CasesTimeSeries))
+
+	w.Write([]byte("Number of results:" + strconv.Itoa(len(res.CasesTimeSeries))))
+}
+
+func main() {
+	port := "3000"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":"+port, mux)
 }
