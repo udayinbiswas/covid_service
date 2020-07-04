4a99fe7012168aa643385685d3cc1441a350ecf3
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-07-04 21:26:43 +0530

    added statewise and tested data

--- external/crowdsource.go
+++ external/crowdsource.go
@@ -13,6 +13,8 @@ const (
 // CrowdSource ...
 type CrowdSource struct {
 	CasesTimeSeries []CasesTimeSeries `json:"cases_time_series"`
+	StateWise       []StateWise       `json:"statewise"`
+	Tested          []Tested          `json:"tested"`
 }
 
 // CasesTimeSeries ...
@@ -24,6 +26,40 @@ type CasesTimeSeries struct {
 	TotalConfirmed string `json:"totalconfirmed"`
 	TotalDeceased  string `json:"totaldeceased"`
 	TotalRecovered string `json:"totalrecovered"`
+}
+
+// StateWise ...
+type StateWise struct {
+	Active          string `json:"active"`
+	Confirmed       string `json:"confirmed"`
+	Deaths          string `json:"deaths"`
+	DeltaConfirmed  string `json:"deltaconfirmed"`
+	DeltaDeaths     string `json:"deltadeaths"`
+	DeltaRecovered  string `json:"deltarecovered"`
+	LastUpdatedTime string `json:"lastupdatetime"`
+	MigratedOther   string `json:"migratedother"`
+	Recovered       string `json:"recovered"`
+	State           string `json:"state"`
+	StateCode       string `json:"statecode"`
+	StateNotes      string `json:"statenotes"`
+}
+
+// Tested ...
+type Tested struct {
+	IndividualsTestedPerConfirmedCase string `json:"individualstestedperconfirmedcase"`
+	PositiveCasesFromSamplesReported  string `json:"positivecasesfromsamplesreported"`
+	SampleReportedToday               string `json:"samplereportedtoday"`
+	Source                            string `json:"source"`
+	Source1                           string `json:"source1"`
+	TestedAsOf                        string `json:"testedasof"`
+	TestPositivityRate                string `json:"testpositivityrate"`
+	TestsConductedByPrivateLabs       string `json:"testsconductedbyprivatelabs"`
+	TestsPerConfirmedCase             string `json:"testsperconfirmedcase"`
+	TestsPerMillion                   string `json:"testspermillion"`
+	TotalIndividualsTested            string `json:"totalindividualstested"`
+	TotalPositiveCases                string `json:"totalpositivecases"`
+	TotalSamplesTested                string `json:"totalsamplestested"`
+	UpdateTimestamp                   string `json:"updatetimestamp"`
 }
 
 // GetCrowdSourcedData ...
